40504dc609ff3a60b9c29d8290aace55d7fbb844
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-08 11:01:34 -0300

    Update react-dio

--- Components/react-dio/src/App.js
+++ Components/react-dio/src/App.js
@@ -1,17 +1,19 @@
+import Item from './components/Item'
+
 const App = () => {
   return(
     <>
       <h1>Primeira aplicação com React</h1>
       <ul>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-            Item 1
-        </a>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-            Item 2
-        </a>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">
-            Item 3
-        </a>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3
+        </Item>
       </ul>
     </>
   )
