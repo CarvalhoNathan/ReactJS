f4d0a18498d99b7c6a2fd6cd60817bbc9732bfaa
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-21 20:11:49 -0300

    Adding Get, Post, Put and Delete

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de rotas
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'bar '});
+  res.status(200).send({ foo: 'Sucesso! '});
 });
 
+// Inicialização do servidor
 app.listen(3000, () => {
   console.log('Aplicação executando a porta 3000')
 });
