4cbf113639bb960b81342182010494ae7576d2fe
CarvalhoNathan <fariasnathan75@gmail.com>
2022-02-24 00:05:48 -0300

    Update adding update and delete user

--- Node/live-02/src/routes/users.route.ts
+++ Node/live-02/src/routes/users.route.ts
@@ -29,18 +29,22 @@ usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction
 
 // put /users/:uuid
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
   const modifiedUser = req.body;
 
   modifiedUser.uuid = uuid;
 
-  res.status(StatusCodes.OK).send(modifiedUser);
+  await userRepository.update(modifiedUser);
+
+  res.status(StatusCodes.OK).send();
 });
 
 // delete /users/:uuid
 
-usersRoute.delete('/users/:uuid',(req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  await userRepository.remove(uuid);
   res.sendStatus(StatusCodes.OK);
 });
 
